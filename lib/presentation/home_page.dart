f0fe6a2b1a8e747ee80ed0619bd87fc34bd78f31
Manan Gandhi <ardumanan@gmail.com>
2024-01-12 21:37:16 +0530

    Added vision

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,10 +17,16 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController _promptController = TextEditingController();
   List messages = [];
   String result = "";
+  List<Uint8List> images = [];
+  bool isBusy = false;
 
   void newMessage(String text) async {
-    var res = await gemini.text(text);
     setState(() {
+      isBusy = true;
+    });
+    var res = await gemini.textAndImage(text: text, images: images);
+    setState(() {
+      isBusy = false;
       result = res?.output ?? "No Output";
     });
   }
@@ -28,26 +38,58 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            if (images.isNotEmpty)
+              SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: images.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.memory(images[index]),
+                    );
+                  },
+                ),
+              ),
             Expanded(
               child: Center(
-                child: Text(result),
-              ),
+                child: isBusy
+                    ? const CircularProgressIndicator()
+                    : Text(result),
+              )
             ),
             Row(
               children: [
+                IconButton(
+                  onPressed: () async {
+                    final image = await ImagePicker().pickImage(
+                      source: ImageSource.gallery,
+                    );
+                    if (image != null) {
+                      debugPrint(image.path);
+                      setState(() {
+                        images.add(File(image.path).readAsBytesSync());
+                      });
+                    }
+                  },
+                  icon: const Icon(Icons.file_upload_outlined),
+                ),
                 Expanded(
                   child: TextField(
                     controller: _promptController,
                     decoration: const InputDecoration(
-                      label: Text("Your message"),
+                      label: Text("Your prompt"),
                     ),
                   ),
                 ),
                 IconButton(
                   onPressed: () {
                     newMessage(_promptController.text);
+                    _promptController.clear();
                   },
                   icon: const Icon(Icons.send),
+                  enableFeedback: true,
                 ),
               ],
             ),
