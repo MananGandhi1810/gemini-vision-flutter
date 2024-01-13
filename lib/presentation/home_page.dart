78d956ee68ee2218c5712005fdcd8b2a5a03ba87
Manan Gandhi <ardumanan@gmail.com>
2024-01-13 09:39:40 +0530

    Added camera option, and some ui changes

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -19,6 +19,8 @@ class _HomePageState extends State<HomePage> {
   String result = "";
   List<Uint8List> images = [];
   bool isBusy = false;
+  ImageSource _source = ImageSource.camera;
+  bool _cancel = false;
 
   void newMessage(String text) async {
     setState(() {
@@ -34,66 +36,132 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            if (images.isNotEmpty)
-              SizedBox(
-                height: 100,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: images.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.memory(images[index]),
-                    );
-                  },
-                ),
-              ),
-            Expanded(
-              child: Center(
-                child: isBusy
-                    ? const CircularProgressIndicator()
-                    : Text(result),
-              )
-            ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () async {
-                    final image = await ImagePicker().pickImage(
-                      source: ImageSource.gallery,
-                    );
-                    if (image != null) {
-                      debugPrint(image.path);
-                      setState(() {
-                        images.add(File(image.path).readAsBytesSync());
-                      });
-                    }
-                  },
-                  icon: const Icon(Icons.file_upload_outlined),
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: _promptController,
-                    decoration: const InputDecoration(
-                      label: Text("Your prompt"),
-                    ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              if (images.isNotEmpty)
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: images.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Stack(
+                          children: [
+                            Image.memory(images[index]),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: CircleAvatar(
+                                radius: 15,
+                                backgroundColor: Colors.black,
+                                child: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      images.removeAt(index);
+                                    });
+                                  },
+                                  icon: const Icon(
+                                    Icons.close,
+                                    color: Colors.white,
+                                    size: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ),
-                IconButton(
-                  onPressed: () {
-                    newMessage(_promptController.text);
-                    _promptController.clear();
-                  },
-                  icon: const Icon(Icons.send),
-                  enableFeedback: true,
-                ),
-              ],
-            ),
-          ],
+              Expanded(
+                  child: Center(
+                child: isBusy ? const CircularProgressIndicator() : Text(result),
+              )),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () async {
+                      await showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text("Select Image"),
+                          content: Text(
+                              "Select an image from gallery or take a photo"),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                setState(() {
+                                  _source = ImageSource.gallery;
+                                });
+                              },
+                              child: Text("Gallery"),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                setState(() {
+                                  _source = ImageSource.camera;
+                                });
+                              },
+                              child: Text("Camera"),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                setState(() {
+                                  _cancel = true;
+                                });
+                              },
+                              child: Text("Cancel"),
+                            ),
+                          ],
+                        ),
+                      );
+                      if (_cancel) {
+                        setState(() {
+                          _cancel = false;
+                        });
+                        return;
+                      }
+                      final image = await ImagePicker().pickImage(
+                        source: _source,
+                      );
+                      if (image != null) {
+                        debugPrint(image.path);
+                        setState(() {
+                          images.add(File(image.path).readAsBytesSync());
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.file_upload_outlined),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _promptController,
+                      decoration: const InputDecoration(
+                        label: Text("Your prompt"),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      newMessage(_promptController.text);
+                      _promptController.clear();
+                    },
+                    icon: const Icon(Icons.send),
+                    enableFeedback: true,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
